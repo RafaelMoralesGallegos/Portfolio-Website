b7ae13a2ddaccd78a9d951720c0827e6bc8711f9
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-05-06 11:20:18 -0600

    update for .ico

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, send_from_directory
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
@@ -8,6 +10,15 @@ bootstrap = Bootstrap5(app)
 @app.route("/")
 def get_home():
     return render_template("index.html")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static/assets"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
 if __name__ == "__main__":
